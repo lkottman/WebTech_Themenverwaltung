5b1eca1b4f8be65a0d5b05e497c721f3aa78f880
svenpetersen <sven.petersen911@gmail.com>
2020-07-18 18:33:58 +0200

    some files deleted

--- Gruppe_1_Registrierung/Login_etc_Dziersan/public/Sven_Louis/adminAnsicht.js
+++ Gruppe_1_Registrierung/Login_etc_Dziersan/public/Sven_Louis/adminAnsicht.js
@@ -3,22 +3,30 @@ const connection = require('../../../.././getConnectionDatabase.js');
 
 
 function getUsers(){
+    let sql = "SELECT name, surname, e_mail, password, course, authorization FROM USER;";
+    connection.query(sql,
+        function (err, result)
+        {
+            if (err)
+                throw err;
 
+            console.log("succes");
+        });
 }
-
-
-
-let sql = "SELECT name, surname, e_mail, password, course, authorization FROM USER;";
-connection.query(sql,
-    function (err, result)
-    {
-        if (err)
-            throw err;
-        console.log("succes");
-    });
 
 
 function changeFieldStatus(input) {
     var textarea = document.getElementById(input);
     textarea.readOnly = !textarea.readOnly;
+}
+
+function changeEntries() {
+    let sql = "UPDATE USER SET name = '', surname = '', e_mail = '', password ='', course='', authorization='' " +
+        "WHERE e_mail = ''";
+    connection.query(sql,
+        function (err, result) {
+            if (err) throw err;
+            if (result)
+                console.log("Erfolgreich")
+        })
 }
