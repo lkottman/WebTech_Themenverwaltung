8a6187177ca4a9ec598003258f1460e0904da210
svenpetersen <sven.petersen911@gmail.com>
2020-07-18 17:49:20 +0200

    some files deleted

--- Gruppe_1_Registrierung/Login_etc_Dziersan/public/Sven_Louis/adminAnsicht.js
+++ Gruppe_1_Registrierung/Login_etc_Dziersan/public/Sven_Louis/adminAnsicht.js
@@ -1,4 +1,21 @@
+const connection = require('../../../.././getConnectionDatabase.js');
 
+
+
+function getUsers(){
+
+}
+
+
+
+let sql = "SELECT name, surname, e_mail, password, course, authorization FROM USER;";
+connection.query(sql,
+    function (err, result)
+    {
+        if (err)
+            throw err;
+        console.log("succes");
+    });
 
 
 function changeFieldStatus(input) {
--- getConnectionDatabase.js
+++ getConnectionDatabase.js
@@ -3,13 +3,16 @@ const path = require("./config/pathConfig.json");
 const mysql = require('mysql');
 const config = JSON.parse(fs.readFileSync(path.path + "/config/datenbankConfig.json"));
 
-let connection = mysql.createConnection(
-    {
-        host: config.host,
-        user: config.user,
-        password: config.password,
-        database: config.database
-    }
-);
 
-module.exports = connection;
+function getConnection(){
+   return  mysql.createConnection(
+        {
+            host: config.host,
+            user: config.user,
+            password: config.password,
+            database: config.database
+        }
+    );
+
+};
+module.exports = getConnection();
