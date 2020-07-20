c2a6bb633c907a6fdec2ad691210680eb3418f22
svenpetersen <sven.petersen911@gmail.com>
2020-07-20 18:36:22 +0200

    hi

--- Gruppe_1_Registrierung/public/routes/register/routesRegister.js
+++ Gruppe_1_Registrierung/public/routes/register/routesRegister.js
@@ -1,19 +1,36 @@
 const express = require('express')
+const mysql = require('mysql')
 const router = express.Router()
+const fs = require('fs');
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const config = JSON.parse(fs.readFileSync("/Users/svenpetersen/IdeaProjects/WebTech_Themenverwaltung_Closly_Finished/config/datenbankConfig.json"));
 const {sendMail, getTextConfirmationEmail, getMailOptions} = require('../nodeMailer/nodeMailer.js');
-const connection = require("../../../../getConnectionDatabase");
+
+const redirect = require("../routesRedirect");
+
+let connection = mysql.createConnection(
+    {
+        host: config.host,
+        user: config.user,
+        password: config.password,
+        database: config.database
+    }
+);
 
 function validateEmail(email) {
     return /^\"?[\w-_\.]*\"?@hs-osnabrueck\.de$/.test(email);
 }
 
-router.post("/register", (request, response) => {
+router.post("/register", redirect.redirectHome, (request, response) => {
 
-    // if(request.method == "OPTIONS"){
-    //     response.set('Access-Control-Allow-Origin', '*');
-    //     response.set('Access-Control-Allow-Headers', 'Content-Type');
-    //     response.status(204).send('');
-    // }
+    console.log(request.body);
+
+    if(request.method == "OPTIONS"){
+        response.set('Access-Control-Allow-Origin', '*');
+        response.set('Access-Control-Allow-Headers', 'Content-Type');
+        response.status(204).send('');
+    }
 
     if(validateEmail(request.body.email === false)){
         response.json({register: "Nur E-Mail Adressen mit der Endung '@hs-osnabrueck.de' sind zugelassen."});
@@ -21,19 +38,18 @@ router.post("/register", (request, response) => {
 
     let servertime = new Date();
     let randomtoken = Math.random().toString(36).substr(2, 6);
-    var message = "";
 
     //Check if used token is valid
     connection.query("SELECT * FROM TOKEN WHERE " +
         'gentoken = "' + request.body.token + '"',
-        function (err, result, fields) {
+        function (err, result, fields)
+        {
             if (err)
                 throw err;
-            else {
-                const length = result.length;
-
-                if (result.length === 1) {
-
+            else
+                {
+                if (result.length !== 0)
+                {
                     let startTime = result[0].start;
                     let endTime = result[0].end;
                     let token = result[0].gentoken;
@@ -41,14 +57,19 @@ router.post("/register", (request, response) => {
 
                     if (token === clientToken
                         && servertime >= startTime
-                        && servertime <= endTime) {
-                        connection.query("SELECT * FROM USER WHERE " + 'e_mail = "'
-                            + request.body.email + '"',
-                            function (err, result) {
-                                if (err) {
+                        && servertime <= endTime)
+                    {
+                        connection.query("SELECT * FROM USER WHERE " + " e_mail = '"
+                            + request.body.email + "';",
+                            function (err, result)
+                            {
+                                if (err)
+                                {
                                     throw err;
-                                } else {
-                                    if (result.length === 0) {
+                                } else
+                                    {
+                                    if (result.length === 0)
+                                    {
                                         connection.query("INSERT INTO USER(token,name,surname,"
                                             + "e_mail,password,confirm_token ,verified, authorization) VALUES("
                                             + '"' + request.body.token + '",'
@@ -59,37 +80,38 @@ router.post("/register", (request, response) => {
                                             + '"' + randomtoken + '",'
                                             + '' + "false" + ','
                                             + '"student"' + ')',
-                                            function (err) {
+                                            function (err)
+                                            {
                                                 if (err)
                                                     throw err;
-                                                else {
+                                                else
+                                                    {
                                                     console.log("User created");
-                                                    // response.redirect("/successfullregistration");
-
-                                                    response.json({register: ""});
-
                                                     sendMail(getMailOptions(request.body.email,
                                                         'E-Mail bestätigen!!',getTextConfirmationEmail(randomtoken,
-                                                            request.body.email, request.body.name)));
+                                                            request.body.email, request.body.name)))
+                                                     }
+                                                 });
+                                            } else
 
-                                                };
-                                            });
-                                    } else {
                                         response.json({register: "Fehlgeschlagen: Benutzer existiert bereits."});
+                                        }
                                     }
-                                }
-                            });
-                    } else {
-                        response.json({register: "Fehlgeschlagen: Freischaltcode ist abgelaufen."});
-                    }
-                } else {
-                    if (length === 0){
-                        response.json({register: "Fehlgeschlagen: Freischaltcode existiert nicht."});
+                                });
+                            }
+                        else
+                        {
+                        response.json({register: "Freischaltcode ist abgelaufen."});
+                        }
+
+                else
+                    {
+                        response.json({register: "Freischaltcode existiert nicht."});
                     }
                 }
-            }
-        });
-});
+            })
+        }
+    );
 
 
 module.exports = router;
