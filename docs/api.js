f1406ccb34f217dbdb66b2262dfea2131f702e71
Dziersan <dominik.dziersan@web.de>
2020-07-24 01:01:41 +0200

    Bugfix

---
+++ docs/api.js
@@ -0,0 +1,26 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Client side from login",
+        "Client side from register",
+        "Mainpage from this project",
+        "Node.js server",
+        "Server-Side from login",
+        "Server-Side from token",
+        "Tests for login, register and simple functions",
+        "client side from token",
+        "routesLogin, server site processing of the Login process"
+    ],
+    "modules": [
+        "This router is for processing GETs to html files and simple GETs"
+    ],
+    "allModules": [
+        {
+            "displayName": "This router is for processing GETs to html files and simple GETs",
+            "name": "This router is for processing GETs to html files and simple GETs",
+            "description": "Version 1.0\n23.07.2020\nAUTHOR: Dominik Dziersan & Sven Petersen & Louis Kottmann"
+        }
+    ],
+    "elements": []
+} };
+});
