bd7d801f14964f852d12d438dd35360f5421ed88
Dziersan <dominik.dziersan@web.de>
2020-07-25 00:06:39 +0200

    Bugfix

--- docs/api.js
+++ docs/api.js
@@ -7,9 +7,17 @@ YUI.add("yuidoc-meta", function(Y) {
         "Node.js server",
         "Server-Side from login",
         "Server-Side from token",
+        "Simple database connection",
         "Tests for login, register and simple functions",
         "client side from token",
-        "routesLogin, server site processing of the Login process"
+        "for preventing sql-injections",
+        "routesLogin, server site processing of the Login process",
+        "to change the password of a user stores it in the database.",
+        "to generate a object for password changing",
+        "to hash a given string and compares it afterwards",
+        "to receive mail for changing the user's password",
+        "utility class to return parameter from url",
+        null
     ],
     "modules": [
         "This router is for processing GETs to html files and simple GETs"
